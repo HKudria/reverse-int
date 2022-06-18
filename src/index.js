886d8c02ec4021768095944d01a0897a7b95186a
Herman Kudria <h.kudrya@hotmail.com>
2022-06-18 23:35:04 +0200

    feat: write reverse function with passed test in 100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports =function reverse(n) {
+
+    const result = []
+    for (let i = 0; i < n.toString().length; i++) {
+        if(n.toString()[i]!=='-') result.unshift(n.toString()[i])
+    }
+return ~~result.join('');
 }
+
